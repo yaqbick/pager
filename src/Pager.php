33ff6bb2bee4b04259b1961c641c20a8355ef856
yaqbick <yaqbick@gmail.com>
2020-05-20 22:22:59 +0200

    update

--- src/Pager.php
+++ src/Pager.php
@@ -1,7 +1,10 @@
 <?php
 
 declare(strict_types=1);
-namespace Pager\Pager;
+
+namespace Pager;
+
+use Exception;
 
 class Pager
 {
@@ -14,15 +17,20 @@ class Pager
 
     public function __construct(int $numberOfObjects, int  $objectsPerPage)
     {
+        if ($numberOfObjects <= 0 || $objectsPerPage <= 0) {
+            throw new Exception('your input must be greater than 0');
+        }
         $this->numberOfObjects = $numberOfObjects;
         $this->objectsPerPage = $objectsPerPage;
-        $this->numberOfPages = $numberOfObjects/$objectsPerPage;
+        $this->numberOfPages = intval($numberOfObjects / $objectsPerPage);
         $this->currentPage = 1;
+        $this->setPreviousPage();
+        $this->setNextpage();
     }
 
-    public function setPageAsCurrent(int $pageNumber):void
+    public function setPageAsCurrent(int $pageNumber): void
     {
-        if ($pageNumber<1 || $this->numberOfPages<$pageNumber) {
+        if ($pageNumber < 1 || $this->numberOfPages < $pageNumber) {
             throw new Exception($pageNumber.' not exists');
         } else {
             $this->currentPage = $pageNumber;
@@ -31,14 +39,14 @@ class Pager
         }
     }
 
-    public function getNumberOfPages():int
+    public function getNumberOfPages(): int
     {
         return $this->numberOfPages;
     }
 
     public function displayPagination(): array
     {
-        $pages = ['first_page'=>1,'last_page'=> $this->numberOfPages,'current_page'=>$this->currentPage];
+        $pages = ['first_page' => 1, 'last_page' => $this->numberOfPages, 'current_page' => $this->currentPage];
         if ($this->getPreviousPage()) {
             $pages['previous_page'] = $this->getPreviousPage();
         }
@@ -49,37 +57,36 @@ class Pager
         return $pages;
     }
 
-
-    public function getCurrentPage():int
+    public function getCurrentPage(): int
     {
         return $this->currentPage;
     }
 
-    public function getPreviousPage():int
+    public function getPreviousPage(): int
     {
         return $this->previousPage;
     }
 
-    public function getNextPage():int
+    public function getNextPage(): int
     {
         return $this->nextPage;
     }
 
-    private function setPreviousPage():void
+    private function setPreviousPage(): void
     {
-        if ($pageNumber -1 <=0) {
+        if ($this->currentPage - 1 <= 0) {
             $this->previousPage = 0;
         } else {
-            $this->previousPage = $pageNumber -1;
+            $this->previousPage = $this->currentPage - 1;
         }
     }
 
-    private function setNextPage():void
+    private function setNextPage(): void
     {
-        if ($pageNumber +1 > $this->numberOfPages) {
+        if ($this->currentPage + 1 > $this->numberOfPages) {
             $this->nextPage = 0;
         } else {
-            $this->nextPage = $pageNumber +1;
+            $this->nextPage = $this->currentPage + 1;
         }
     }
 }
--- tests/PagerTest.php
+++ tests/PagerTest.php
@@ -1,12 +1,30 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use Pager\Pager;
 
 final class PagerTest extends TestCase
 {
-    public function previousPageIsEqualZeroWhenCurrentPageIsOne()
+    /** @test */
+    public function previousPageIsEqualZeroWhenCurrentPageIsOne(): void
     {
         $pager = new Pager(10, 5);
         $this->assertEquals(0, $pager->getPreviousPage());
+    }
+
+    /** @test */
+    public function nextPageIsEqualZeroWhenCurrentPageIsLast(): void
+    {
+        $pager = new Pager(10, 5);
+        $pager->setPageAsCurrent(2);
+        $this->assertEquals(0, $pager->getNextPage());
+    }
+
+    /** @test */
+    public function pageNumberIsAlwaysInteger()
+    {
+        $pager = new Pager(10, 3);
+        $this->assertIsInt($pager->getNumberOfPages());
     }
 }
